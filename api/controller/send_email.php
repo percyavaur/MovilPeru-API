0310e777ce39abc5ddcdcbeac6d0d814949f3554
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-06-27 03:37:17 -0500

    enviar correo con phpmailer

--- api/controller/send_email.php
+++ api/controller/send_email.php
@@ -6,24 +6,49 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../model/down/email.php';
+require '../libs/PHPMAILER/PHPMailer.php';
+require '../libs/PHPMAILER/SMTP.php';
+require '../libs/PHPMAILER/Exception.php';
+require '../libs/PHPMAILER/OAuth.php';
+// include_once '../model/down/email.php';
 
-$email = new Email();
+// $email = new Email();
+$email = new PHPMailer\PHPMailer\PHPMailer();
+
+$mail->isSMTP();
+
 
 $data = json_decode(file_get_contents("php://input"));
 
-$email->destinatario = $data->destinatario;
-$email->ticket = $data->ticket;
+$correoDestinatario = $data->destinatario;
+$ticket = $data->ticket;
+
+$mail->SMTPDebug = 2;                                       // Enable verbose debug output
+$mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+$mail->Port       = 587;                                    // TCP port to connect to
+$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+$mail->Username   = 'movilperu.info@gmail.com';             // SMTP username
+$mail->Password   = 'jueves2706';                           // SMTP password
+
+//Recipients
+$mail->setFrom('movilperu.info@gmail.com', 'Movil Peru');
+$mail->addAddress($correoDestinatario, 'Usuario de Movil Peru');     // Add a recipient
+
+// Content
+$mail->Subject = 'Movil Peru - Su Reserva ha sido exitosa';
+$mail->Body    = 'Se ha realizado una Reserva de un Viaje con número de ticket ' + $ticket;
+$mail->isHTML(true);                                         // Set email format to HTML
 
 $array = [];
-if ($email->send_mail()) {
-    $array["success"] = true;
-    echo json_encode($array);
-
-} else {
-
+if(!$mail->send()){
     $array["message"] = "Error al enviar correo";
     $array["success"] = false;
+
+    echo json_encode($array);
+}else{
+    $array["message"] = "El correo ha sido enviado correctamente";
+    $array["success"] = true;
 
     echo json_encode($array);
 }
