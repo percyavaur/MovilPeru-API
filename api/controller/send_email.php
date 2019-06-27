27fc67ed0c5e1432c757d0b6e00cbc32a6df218e
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-06-27 10:51:04 -0500

    this

--- api/controller/send_email.php
+++ api/controller/send_email.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+    $data = json_decode(file_get_contents("php://input"));
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
@@ -13,7 +15,6 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
     require 'PHPMailer/PHPMailer.php';
     require 'PHPMailer/SMTP.php';
 
-    $data = json_decode(file_get_contents("php://input"));
     $array = [];
 
     // Instantiation and passing `true` enables exceptions
@@ -31,11 +32,11 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
         //Recipients
         $mail->setFrom('movilperu.info@gmail.com', 'Movil Peru');
-        $mail->addAddress($this->destinatario, 'Usuario de Movil Peru');     // Add a recipient
+        $mail->addAddress($data->destinatario, 'Usuario de Movil Peru');     // Add a recipient
 
         // Content
         $mail->Subject = 'Movil Peru - Su Reserva ha sido exitosa';
-        $mail->Body    = 'Se ha realizado una Reserva de un Viaje con número de ticket ' + $this->ticket;
+        $mail->Body    = 'Se ha realizado una Reserva de un Viaje con número de ticket ' + $data->ticket;
         $mail->isHTML(true);                                         // Set email format to HTML
 
         $mail->send();
