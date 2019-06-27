eeb0f02af82080fbd2e2c448582f69ce65fe5753
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-06-27 02:56:16 -0500

    enviar correo

---
+++ api/controller/send_email.php
@@ -0,0 +1,30 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin:  *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../model/down/email.php';
+
+$email = new Email();
+
+$data = json_decode(file_get_contents("php://input"));
+
+$email->destinatario = $data->destinatario;
+$email->ticket = $data->ticket;
+
+$array = [];
+if ($email->send_mail()) {
+    $array["message"] = "Noticia creada correctamente";
+    $array["success"] = true;
+    echo json_encode($array);
+
+} else {
+
+    $array["message"] = "Error al crear noticia";
+    $array["success"] = false;
+
+    echo json_encode($array);
+}
---
+++ api/model/down/email.php
@@ -0,0 +1,30 @@
+<?php
+
+class news
+{
+    public $nombre;
+    public $correo;
+    public $telefono;
+    public $ticket;
+    public $destinatario;
+    public $asunto;
+    public $carta;
+
+    public function __construct()
+    {
+        $this->asunto = "Movil Peru - Su reserva ha sido exitosa";
+        $this->nombre = "Movil Peru";
+        $this->correo = "movilperu@gmail.com";
+        $this->telefono = "956727151";
+    }
+
+    function send_mail()
+    {
+        $this->carta = "De: $this->nombre \n";
+        $this->carta .= "Correo: $this->correo \n";
+        $this->carta .= "Telefono: $this->telefono \n";
+        $this->carta .= "Mensaje: Su Reserva realizada con número de ticket $this->ticket, ha sido exitosa.  \n";
+
+        mail($this->destinatario,$this->asunto,$this->carta);
+    }
+}
