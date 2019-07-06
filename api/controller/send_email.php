d28b554017220bf33875a17b91b0f47e04e4e276
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-07-05 20:05:46 -0500

    agregando destinatario y ticket

--- api/controller/send_email.php
+++ api/controller/send_email.php
@@ -1,4 +1,10 @@
 <?php
+// required headers
+header("Access-Control-Allow-Origin:  *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -6,6 +12,11 @@ use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
 require '../vendor/autoload.php';
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$usuario = $data->destinatario;
+$ticket = $data->ticket;
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -22,9 +33,9 @@ try {
     $mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('movilperu.info@gmail.com');
+    $mail->setFrom('movilperu.info@gmail.com','Movil Perú');
     // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('oscarmbravoc@gmail.com');               // Name is optional
+    $mail->addAddress($usuario);               // Name is optional
     // $mail->addReplyTo('info@example.com', 'Information');
     // $mail->addCC('cc@example.com');
     // $mail->addBCC('bcc@example.com');
@@ -35,8 +46,8 @@ try {
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Ha realizado una reserva con Movil Perú';
+    $mail->Body    = "Usted ha realizado una reserva con el número de ticket: <b style='color:red;'>$ticket</b>";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
