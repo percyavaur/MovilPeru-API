a24781b939a5e505712406ea46ef259ceff4e93c
percyavaur <percyavaur@hotmail.com>
2019-07-11 11:42:33 -0500

    add pasajes info general by venta
    add pasajes info general by venta

--- api/controller/token_culqi.php
+++ api/controller/token_culqi.php
@@ -1,8 +1,10 @@
 <?php
-/**
- * Ejemplo 1
- * Como crear un token a una tarjeta Culqi PHP.
- */
+header("Access-Control-Allow-Origin:  *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 
 try {
   // Usando Composer (o puedes incluir las dependencias manualmente)
--- api/model/down/pasaje.php
+++ api/model/down/pasaje.php
@@ -18,6 +18,11 @@ class pasaje
     public $vueltaOrigen;
     public $vueltaDestino;
     public $fechaCompra;
+    public $idaFecha;
+    public $idaHora;
+    public $vueltaFecha;
+    public $vueltaHora;
+    public $conductor;
     public $pasajesa = array();
 
     public function __construct($db)
@@ -70,6 +75,67 @@ class pasaje
                     'vueltaOrigen' => $this->vueltaOrigen,
                     'vueltaDestino' => $this->vueltaDestino,
                     'fechaCompra' => $this->fechaCompra
+                );
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function tripInfoPassenger()
+    {
+
+        $query = "SELECT * FROM tripInfoPassenger WHERE idVenta = ?";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(1, $this->idVenta);
+
+        if ($stmt->execute()) {
+
+            $num = $stmt->rowCount();
+
+            for ($i = 0; $i < $num; $i++) {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                // assign values to object properties
+                $this->idPasaje = $row['idPasaje'];
+                $this->idUsuario = $row['idUsuario'];
+                $this->comprador = $row['comprador'];
+                $this->idCliente = $row['idCliente'];
+                $this->tipoDocumento = $row['tipoDocumento'];
+                $this->numDocumento = $row['numDocumento'];
+                $this->apellidos = $row['apellidos'];
+                $this->nombres = $row['nombres'];
+                $this->idaOrigen = $row['idaOrigen'];
+                $this->idaDestino = $row['idaDestino'];
+                $this->vueltaOrigen = $row['vueltaOrigen'];
+                $this->vueltaDestino = $row['vueltaDestino'];
+                $this->fechaCompra = $row['fechaCompra'];
+                $this->idaFecha = $row['idaFecha'];
+                $this->idaHora = $row['idaHora'];
+                $this->vueltaFecha = $row['vueltaFecha'];
+                $this->vueltaHora = $row['vueltaHora'];
+                $this->conductor = $row['conductor'];
+
+                $this->pasajesa[$i] = array(
+                    'idPasaje' => $this->idPasaje,
+                    'idVenta' => $this->idVenta,
+                    'idUsuario' => $this->idUsuario,
+                    'comprador' => $this->comprador,
+                    'idCliente' => $this->idCliente,
+                    'tipoDocumento' => $this->tipoDocumento,
+                    'numDocumento' => $this->numDocumento,
+                    'apellidos' => $this->apellidos,
+                    'nombres' => $this->nombres,
+                    'idaOrigen' => $this->idaOrigen,
+                    'idaDestino' => $this->idaDestino,
+                    'vueltaOrigen' => $this->vueltaOrigen,
+                    'vueltaDestino' => $this->vueltaDestino,
+                    'fechaCompra' => $this->fechaCompra,
+                    'idaFecha' => $this->idaFecha,
+                    'idaHora' => $this->idaHora,
+                    'vueltaFecha' => $this->vueltaFecha,
+                    'vueltaHora' => $this->vueltaHora,
+                    'conductor' => $this->conductor,
                 );
             }
             return true;
