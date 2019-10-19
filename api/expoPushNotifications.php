0a9afcb03040a6547c106c711e8356e141a33fc4
Trackerpjau <38476784+percyavaur@users.noreply.github.com>
2019-10-19 17:51:52 -0500

    a

--- api/expoPushNotifications.php
+++ api/expoPushNotifications.php
@@ -5,8 +5,8 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-include_once './config/database.php';
-include_once './model/down/expoToken.php';
+include_once __DIR__.'./config/database.php';
+include_once __DIR__.'./model/down/expoToken.php';
 require_once __DIR__.'./vendor/autoload.php';
 
 $database = new Database();
