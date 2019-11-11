503e84bdf171a00b33a64064262a29e2e58fc07e
Trackerpjau <38476784+percyavaur@users.noreply.github.com>
2019-11-10 21:47:32 -0500

    test
    test

--- api/model/down/viaje.php
+++ api/model/down/viaje.php
@@ -181,7 +181,7 @@ class viaje
 
     function getAllTrips()
     {
-        $idViaje_set = !empty($this->idViaje) ? " && idViaje != ? LIMIT 0,1" : "";
+        $idViaje_set = !empty($this->idViaje) ? " && idViaje != ?" : "";
         $query = "SELECT * FROM viajesInfo {$idViaje_set}";
         $stmt = $this->conn->prepare($query);
 
